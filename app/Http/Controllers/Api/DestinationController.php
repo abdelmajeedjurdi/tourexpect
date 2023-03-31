10f1d47e3a9c8a3650ce7e0cdd132fea20ff95fb
Edo Khudeeda <edo.khudeeda@gmail.com>
2023-03-31 13:59:49 +0300

    from locale

--- app/Http/Controllers/Api/DestinationController.php
+++ app/Http/Controllers/Api/DestinationController.php
@@ -28,7 +28,7 @@ class DestinationController extends Controller
     }
     public function getDestinations(Request $request)
     {
-        if ($request) {
+        if (strlen($request->country) > 2) {
             $destinations = DB::table('destinations')
                 ->join('countries', 'destinations.country_id', '=', 'countries.id')->where('countries.slug', $request->country)
                 ->select('destinations.*', 'countries.name_ar as country_ar', 'countries.name_en as country_en', 'countries.slug as country_slug')
